18f11b39116213d7f502887cea38f1ea84184130
Death <113825692+Deathdadev@users.noreply.github.com>
2024-12-10 16:44:50 +1000

    Update torch.js
    Testing to see whether installing with uv, with online requirements.txt for uv will resolve issues with installation of packages.
    
    If this change does not resolve these issues, the next change should use the prebuilt wheels for installation, which only need to be changed because of windows visual studio

--- torch.js
+++ torch.js
@@ -7,7 +7,7 @@ module.exports = {
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
           "python -m pip install --upgrade pip",
-          "pip install --upgrade setuptools wheel"
+          "uv pip install --upgrade setuptools wheel"
         ]
       }
     },
@@ -19,15 +19,22 @@ module.exports = {
         "venv": "{{args && args.venv ? args.venv : null}}",
         "path": "{{args && args.path ? args.path : '.'}}",
         "message": [
-          "pip install torch==2.5.1 torchvision==0.20.1 {{args && args.xformers ? 'xformers' : ''}}  --index-url https://download.pytorch.org/whl/cu124",
-          "pip install https://github.com/bdashore3/flash-attention/releases/download/v2.7.1.post1/flash_attn-2.7.1.post1+cu124torch2.5.1cxx11abiFALSE-cp310-cp310-win_amd64.whl",
-          // "pip install flash-attn",
-          "pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu124.html",
-          "pip install git+https://github.com/NVlabs/nvdiffrast.git",
-          "pip install git+https://github.com/JeffreyXiang/diffoctreerast.git",
-          "pip install git+https://github.com/autonomousvision/mip-splatting.git#subdirectory=submodules/diff-gaussian-rasterization",
-          "pip install -e extensions/vox2seq/",
-          "pip install spconv-cu120"
+          "uv pip sync https://raw.githubusercontent.com/sdbds/TRELLIS-for-windows/refs/heads/main/requirements-uv.txt --index-strategy unsafe-best-match",
+          "uv pip install --no-build-isolation -e extensions/vox2seq/",
+          "uv pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu124.html"
+          "uv pip install --no-build-isolation git+https://github.com/JeffreyXiang/diffoctreerast.git"
+          "uv pip install git+https://github.com/sdbds/diff-gaussian-rasterization"
+          
+          // These have been commented out for the sake of checking whether syncing from online requirements.txt will resolve issues or not
+          // "pip install torch==2.5.1 torchvision==0.20.1 {{args && args.xformers ? 'xformers' : ''}}  --index-url https://download.pytorch.org/whl/cu124",
+          // "pip install https://github.com/bdashore3/flash-attention/releases/download/v2.7.1.post1/flash_attn-2.7.1.post1+cu124torch2.5.1cxx11abiFALSE-cp310-cp310-win_amd64.whl",
+          // // "pip install flash-attn",
+          // "uv pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.5.1_cu124.html",
+          // "pip install git+https://github.com/NVlabs/nvdiffrast.git",
+          // "pip install git+https://github.com/JeffreyXiang/diffoctreerast.git",
+          // "pip install git+https://github.com/autonomousvision/mip-splatting.git#subdirectory=submodules/diff-gaussian-rasterization",
+          // "pip install -e extensions/vox2seq/",
+          // "pip install spconv-cu120"
         ]
       }
     },
